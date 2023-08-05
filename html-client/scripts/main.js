81fd01b591d854ec846b8cc26c539852fceb114d
Okpainmo <okpainmoandrew@gmail.com>
2023-08-05 13:11:14 +0100

    assets, refactor: I replaced the svg code for delete icon with a delete icon image to resolve click the failure currently being experienced

--- html-client/scripts/main.js
+++ html-client/scripts/main.js
@@ -62,11 +62,7 @@ const handleGetProfiles = async function () {
                     <path d="M5.25 5.25a3 3 0 00-3 3v10.5a3 3 0 003 3h10.5a3 3 0 003-3V13.5a.75.75 0 00-1.5 0v5.25a1.5 1.5 0 01-1.5 1.5H5.25a1.5 1.5 0 01-1.5-1.5V8.25a1.5 1.5 0 011.5-1.5h5.25a.75.75 0 000-1.5H5.25z" />
                   </svg>
                 </a>
-                <button data-id="${_id}">
-                  <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                    <path class="delete-profile-icon" fill-rule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z" clip-rule="evenodd" />
-                  </svg>
-                </button>
+                <img src="./assets/images/delete-icon.png" class="delete-profile-icon w-6 cursor-pointer" alt="delete-icon" data-id="${_id}"/>
               </div>
             </section>
             <section class="flex flex-col justify-between min-h-[200px]">
@@ -118,7 +114,7 @@ async function handleShowOverlay() {
     showOverlayButtons.forEach((each) => {
       each.addEventListener('click', (e) => {
         modalOverlay.style.display = 'flex';
-        taskId = e.target.parentElement.parentElement.dataset.id;
+        taskId = e.target.dataset.id;
         // console.log(taskId);
       });
     });
